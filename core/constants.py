a23de2b98980c384ecc69f991532d7d202dcac4f
Max Smirnov <makssabdal@gmail.com>
2026-06-01 12:42:23 +0300

    Add constants configuration

---
+++ core/constants.py
@@ -0,0 +1,39 @@
+"""
+Application constants and configurations.
+"""
+
+# Cake names and types
+CAKE_NAMES = {
+    "cake_1": "Банан-карамель",
+    "cake_2": "Баунти",
+    "cake_3": "Молочная девочка",
+    "cake_4": "Красный бархат",
+    "cake_5": "Фисташка-малина",
+    "cake_6": "Медовик",
+    "cake_7": "Сникерс",
+    "cake_8": "Черный лес"
+}
+
+# Available cake sizes
+SIZES = {
+    "size_standard": "Стандарт",
+    "size_bento": "Бенто"
+}
+
+# Delivery options
+LOGISTICS = {
+    "logistics_pickup": "Самовывоз",
+    "logistics_delivery": "Доставка",
+}
+
+# Order limits
+MAX_ORDERS_PER_DATE = 3
+MIN_DAYS_FOR_ORDER = 3
+
+# Calendar settings
+CALENDAR_PAGE_SIZE = 10
+
+# User profile photo ID for about section
+OWNER_PHOTO_ID = (
+    "AgACAgIAAxkBAAIJGmob31lGNiN9qux8qVYFTGuVK1RdAAI1HGsb1pfYSH2RxgvNHuh-AQADAgADeQADOwQ"
+)
